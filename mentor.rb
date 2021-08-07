6b7e6d7ecb552011af5eb527d95111350201d993
kazuhiro.osugi <kazu1914hiro@outlook.jp>
2021-08-07 05:00:12 +0000

    first commit

---
+++ mentor.rb
@@ -0,0 +1,36 @@
+# クラス定義
+class Mentor
+
+# インスタンスが持つ変数（値）
+  attr_accessor :name
+
+ # インスタンスを初期化するための、特別なメソッド
+ def initialize(name)
+    self.name = name
+ end
+
+  # インスタンスが持つメソッド（処理）
+ def job
+    puts "#{self.name}です。私は現役のITプロフェッショナルです。"
+ end
+end
+
+#メンタークラスの継承
+class Rails < Mentor
+
+ def initialize(name)
+   super("赤出")
+ end
+ 
+ def job  
+   puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
+ end  
+end
+
+# インスタンスの生成と、変数への代入
+kirameki = Mentor.new("煌木")
+akaide = Rails.new("赤出")
+
+# インスタンスの使用
+kirameki.job
+akaide.job
